b544d26cf5431287cd27733c3191ea74a8d1dfc1
MichaelWijaya2509 <123831952+michaelwijaya2509@users.noreply.github.com>
2025-12-12 17:32:55 +0700

    search bar fixed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,7 +62,7 @@
             top: 0;
             left: 0;
             right: 0;
-            z-index: 1000;
+            z-index: 100;
             padding: 0;
             height: 70px;
             display: flex;
@@ -125,7 +125,7 @@
 
         /* Logo Image Styling */
         .navbar-brand img {
-            height: 1000px;
+            height: 100px;
             width: auto;
             max-width: 200px;
             object-fit: contain;
@@ -768,7 +768,7 @@
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="/venues">
+                            <a class="nav-link" href="/venue">
                                 <i class="bi bi-basket nav-icon"></i>
                                 <span class="nav-text">Venues</span>
                             </a>
