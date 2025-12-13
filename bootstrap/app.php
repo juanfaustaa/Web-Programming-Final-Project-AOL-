e033f2bc8643167c928c33bd4a02659b5d40b000
ctsfy <korneliusjfp@gmail.com>
2025-12-13 14:28:58 +0700

    coba admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Middleware\IsUser;
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'is_admin' => IsAdmin::class,
+            'is_user'  => IsUser::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
